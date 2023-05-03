93bde389ca9c5a92f3bb7f7301ffa97ca8d2520b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-05-03 10:48:58 +0300

    Add localStorage and IndexedDb examples

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { MainScene } from './MainScene';
 import Phaser from 'phaser';
 import './style.css'
-
+import Dexie from 'dexie';
 var config = {
   type: Phaser.CANVAS,
   width: window.innerWidth,
@@ -18,3 +18,20 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+
+let data = {x: 1000, y: 2000};
+localStorage.setItem('something', JSON.stringify(data));
+
+let localData = JSON.parse(localStorage.getItem('something'));
+console.log(localData);
+
+let db = new Dexie("game");
+db.version(1).stores({
+  entities: `
+    id,
+    name,
+    x,
+    y`,
+});
+
+db.entities.put({id:4, name:'Player',x: 1000,y: 2000})
